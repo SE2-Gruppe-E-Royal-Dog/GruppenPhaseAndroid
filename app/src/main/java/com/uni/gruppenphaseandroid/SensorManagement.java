fd53f9b0e14087550fd01f0ee8aba975327ed4ca
claudialei <100758060+claudialei@users.noreply.github.com>
2022-04-23 13:14:12 +0200

    SensorManager zu SensorReader umbenannt

--- app/src/main/java/com/uni/gruppenphaseandroid/SensorManagement.java
+++
@@ -1,4 +0,0 @@
-package com.uni.gruppenphaseandroid;
-
-public class SensorManagement {
-}
---
+++ app/src/main/java/com/uni/gruppenphaseandroid/SensorReader.java
@@ -0,0 +1,17 @@
+package com.uni.gruppenphaseandroid;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+
+public class SensorReader extends Activity implements SensorEventListener {
+
+        
+}
+
