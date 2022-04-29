b7a509c5ad3be0e01662b5527a6bf14a97c0abb7
Lukas Krall <lukaskr@edu.aau.at>
2022-04-29 20:33:37 +0200

    Instanzvariable cardId und Getter hizugefügt

--- app/src/main/java/com/uni/gruppenphaseandroid/Cards/Card.java
+++ app/src/main/java/com/uni/gruppenphaseandroid/Cards/Card.java
@@ -2,12 +2,18 @@ package com.uni.gruppenphaseandroid.Cards;
 
 public class Card {
     private Cardtype cardtype;
+    private int cardID;
 
-    public Card(Cardtype cardtype){
+    public Card(Cardtype cardtype, int cardID){
         this.cardtype = cardtype;
+        this.cardID = cardID;
     }
 
     public Cardtype getCardtype(){
         return cardtype;
+    }
+
+    public int getCardID(){
+        return cardID;
     }
 }
