733ad4ded7f8449457802da86e1a2971f92e368a
claudialei <100758060+claudialei@users.noreply.github.com>
2022-05-01 11:17:29 +0200

    Method to check if Cheating is allowed

--- app/src/main/java/com/uni/gruppenphaseandroid/Cheater.java
+++ app/src/main/java/com/uni/gruppenphaseandroid/Cheater.java
@@ -4,25 +4,50 @@ package com.uni.gruppenphaseandroid;
 
 public class Cheater {
 
-
     private int roundIndex;
+    private int currentRoundIndex;
     private String playerID;
+    private boolean cheatingAllowed;
+
 
     public Cheater() {
     }
 
     public Cheater(String playerID, int roundIndex){
-        this.playerID = playerID;
-        this.roundIndex = roundIndex;
+        this.playerID = playerID;                             //bis jetzt nur am Server
+        this.roundIndex = 5;                                  //cheating nur alle 5 runden --> wird jede runde -1 gesetzt
+        setCheatingAllowed(false);                            //constructor --> erstes mal schummeln -->
     }
 
 
-    public int getRound() {
+    /**
+     * checks if cheating is permitted --> the player hasn't cheated within 5 rounds
+     */
+    public boolean cheatingAllowed(){
+        if ((getCurrentRoundIndex()-getRoundIndex()) >= 5)
+            setCheatingAllowed(true);
+
+        return isCheatingAllowed();
+    }
+
+
+
+
+    //getter setter
+    public int getRoundIndex() {
         return roundIndex;
     }
 
-    public void setRound(int roundIndex) {
+    public void setRoundIndex(int roundIndex) {
         this.roundIndex = roundIndex;
+    }
+
+    public int getCurrentRoundIndex() {
+        return currentRoundIndex;
+    }
+
+    public void setCurrentRoundIndex(int currentRoundIndex) {
+        this.currentRoundIndex = currentRoundIndex;
     }
 
     public String getPlayerID() {
@@ -31,5 +56,13 @@ public class Cheater {
 
     public void setPlayerID(String playerID) {
         this.playerID = playerID;
+    }
+
+    public boolean isCheatingAllowed() {
+        return cheatingAllowed;
+    }
+
+    public void setCheatingAllowed(boolean cheatingAllowed) {
+        this.cheatingAllowed = cheatingAllowed;
     }
 }
