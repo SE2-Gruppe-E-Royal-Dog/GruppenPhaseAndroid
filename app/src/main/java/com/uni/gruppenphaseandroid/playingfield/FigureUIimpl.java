fc29fdaf82e3e32663cd52c4dde2e115660047fb
Nermin Topalovic <nerminto@edu.aau.at>
2022-05-02 14:47:18 +0200

    issue #34 fixed some visuals

--- app/src/main/java/com/uni/gruppenphaseandroid/playingfield/FigureUIimpl.java
+++ app/src/main/java/com/uni/gruppenphaseandroid/playingfield/FigureUIimpl.java
@@ -3,28 +3,34 @@ package com.uni.gruppenphaseandroid.playingfield;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class FigureUIimpl extends FigureUI {
 
-    private ImageView imageView;
+    private ImageButton imageButton;
     //private View view;
 
     public void createFigureUI(View view, String tag, RelativeLayout relativeLayout, int imageResource){
-        imageView = new ImageButton(view.getContext());
-        imageView.setLayoutParams(new RelativeLayout.LayoutParams(100, 100));
-        relativeLayout.addView(imageView);
-        imageView.setTag(tag);
-        imageView.setImageResource(imageResource);
+        imageButton = new ImageButton(view.getContext());
+        imageButton.setLayoutParams(new RelativeLayout.LayoutParams(140, 140));
+        relativeLayout.addView(imageButton);
+        imageButton.setTag(tag);
+        imageButton.setImageResource(imageResource);
+        //imageButton.setBackgroundColor(0);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageButton.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 
     @Override
     public void moveFigureToPosition(FieldUI targetFieldUI) {
 
-        ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) imageView.getLayoutParams();
+        ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) imageButton.getLayoutParams();
         //marginLayoutParams.setMargins(marginLayoutParams.leftMargin, marginLayoutParams.topMargin, targetFieldUI.getMarginRight(), targetFieldUI.getMarginBottom());
         marginLayoutParams.setMargins(targetFieldUI.getMarginLeft(),targetFieldUI.getMarginTop(),0,0);
-        imageView.setLayoutParams(marginLayoutParams);
+        imageButton.setLayoutParams(marginLayoutParams);
     }
 }
