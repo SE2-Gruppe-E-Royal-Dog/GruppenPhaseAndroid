2fba7fbe246496bd419ada6ecfdea1ecc680a811
Lukas Krall <lukaskr@edu.aau.at>
2022-05-06 21:49:53 +0200

    RequestCardsPayload erbt jetzt von Payload

--- core/src/main/java/com/se2/communication/dto/RequestCardsPayload.java
+++ core/src/main/java/com/se2/communication/dto/RequestCardsPayload.java
@@ -1,24 +1,13 @@
 package com.se2.communication.dto;
 
-public class RequestCardsPayload {
-    int lobbyID;
-    int playerID;
+public class RequestCardsPayload extends Payload{
     boolean sendAll;
     int numOfRequestedCards;
 
     public RequestCardsPayload(int lobbyID, int playerID, boolean sendAll, int numOfRequestedCards) {
-        this.lobbyID = lobbyID;
-        this.playerID = playerID;
+        super(lobbyID, playerID);
         this.sendAll = sendAll;
         this.numOfRequestedCards = numOfRequestedCards;
-    }
-
-    public int getLobbyID() {
-        return lobbyID;
-    }
-
-    public int getPlayerID() {
-        return playerID;
     }
 
     public boolean isSendAll() {
