061297de088e69d42d9135834c6ceb139f5ec65f
Lukas Krall <lukaskr@edu.aau.at>
2022-05-06 20:35:59 +0200

    Updated RequestCardsPayload. Neue Variablen und GEtter hinzugefügt

--- core/src/main/java/com/se2/communication/dto/RequestCardsPayload.java
+++ core/src/main/java/com/se2/communication/dto/RequestCardsPayload.java
@@ -1,10 +1,28 @@
 package com.se2.communication.dto;
 
 public class RequestCardsPayload {
+    int lobbyID;
+    int playerID;
+    boolean sendAll;
     int numOfRequestedCards;
 
-    public RequestCardsPayload(int numOfRequestedCards) {
+    public RequestCardsPayload(int lobbyID, int playerID, boolean sendAll, int numOfRequestedCards) {
+        this.lobbyID = lobbyID;
+        this.playerID = playerID;
+        this.sendAll = sendAll;
         this.numOfRequestedCards = numOfRequestedCards;
+    }
+
+    public int getLobbyID() {
+        return lobbyID;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public boolean isSendAll() {
+        return sendAll;
     }
 
     public int getNumOfRequestedCards() {
