876284fa8c494662385f10ce2bd6f93db532cd9e
sandro <linder.s@gmx.at>
2022-05-09 17:32:36 +0200

    removed assertJ

--- app/src/test/java/com/uni/gruppenphaseandroid/communication/ClientTest.java
+++ app/src/test/java/com/uni/gruppenphaseandroid/communication/ClientTest.java
@@ -1,6 +1,6 @@
 package com.uni.gruppenphaseandroid.communication;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
@@ -39,7 +39,7 @@ public class ClientTest {
 
         verify(clientSpy).send(captor.capture());
 
-        assertThat(captor.getValue()).isEqualTo("{\"type\":\"JOIN_LOBBY\",\"payload\":\"{\\\"playerName\\\":\\\"player-1\\\"}\"}");
+        assertEquals("{\"type\":\"JOIN_LOBBY\",\"payload\":\"{\\\"playerName\\\":\\\"player-1\\\"}\"}", captor.getValue());
     }
 
     @Test
