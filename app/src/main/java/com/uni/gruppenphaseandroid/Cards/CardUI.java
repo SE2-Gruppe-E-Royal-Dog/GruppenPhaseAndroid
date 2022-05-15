08b899a557150472ba5d040a291e26c48d38de00
claudialei <100758060+claudialei@users.noreply.github.com>
2022-05-15 12:29:11 +0200

    created CardUI interface and class

---
+++ app/src/main/java/com/uni/gruppenphaseandroid/Cards/CardUI.java
@@ -0,0 +1,10 @@
+package com.uni.gruppenphaseandroid.Cards;
+
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public interface CardUI {
+
+    public void createCardUI(View view, String tag, RelativeLayout relativeLayout, int imgResource);
+
+}
---
+++ app/src/main/java/com/uni/gruppenphaseandroid/Cards/CardUiIimpl.java
@@ -0,0 +1,11 @@
+package com.uni.gruppenphaseandroid.Cards;
+
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class CardUiIimpl implements CardUI{
+    @Override
+    public void createCardUI(View view, String tag, RelativeLayout relativeLayout, int imgResource) {
+
+    }
+}
