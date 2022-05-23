47d5b26f814af27f888fdc229f6864ba4bcba7d0
sandro <linder.s@gmx.at>
2022-05-23 22:16:42 +0200

    fixed more imports

--- app/src/test/java/com/uni/gruppenphaseandroid/CheaterTest.java
+++ app/src/test/java/com/uni/gruppenphaseandroid/CheaterTest.java
@@ -2,9 +2,7 @@ package com.uni.gruppenphaseandroid;
 
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
 
 import com.uni.gruppenphaseandroid.cheating.Cheater;
 
