6661500ce9da2ef00afbb97a0a8e5ef464805c0f
Nermin Topalovic <nerminto@edu.aau.at>
2022-06-03 22:13:47 +0200

    refactored GamemanagerTests

--- app/src/test/java/com/uni/gruppenphaseandroid/managertests/GamemanagerTest.java
+++ app/src/test/java/com/uni/gruppenphaseandroid/managertests/GamemanagerTest.java
@@ -31,6 +31,13 @@ public class GamemanagerTest {
     FigureManager figureManager;
     Card card;
 
+    Figure figure1;
+    Figure figure2;
+    Field field1;
+    Field field2;
+    UpdateBoardPayload updateBoardPayloadSingleFigure;
+    UpdateBoardPayload updateBoardPayloadTwoFigures;
+
     @Before
     public void setUp() {
         view = mock(View.class);
@@ -38,6 +45,25 @@ public class GamemanagerTest {
         GameManager.getInstance().setPlayingField(playingField);
         figureManager = mock(FigureManager.class);
         card = new Card(Cardtype.EIGTH);
+
+        figure1 = new Figure();
+        figure1.setId(1);
+        figure2 = new Figure();
+        figure2.setId(9);
+        field1 = new Field();
+        field1.setFieldID(3);
+        field2 = new Field();
+        field2.setFieldID(12);
+
+        updateBoardPayloadSingleFigure =  new UpdateBoardPayload(1, -1, 3, -1, 4, 0, "id", "id");
+        updateBoardPayloadTwoFigures = new UpdateBoardPayload(1, 9, 3, 12, 4, 0, "id", "id");
+
+        when(figureManager.getFigureWithID(figure1.getId())).thenReturn(figure1);
+        when(playingField.getFieldWithID(field1.getFieldID())).thenReturn(field1);
+        when(figureManager.getFigureWithID(figure2.getId())).thenReturn(figure2);
+        when(playingField.getFieldWithID(field2.getFieldID())).thenReturn(field2);
+
+        GameManager.getInstance().startGame(4, 0, "id", "id", figureManager);
     }
 
     @After
@@ -48,7 +74,6 @@ public class GamemanagerTest {
 
     @Test
     public void startGameTest() {
-        GameManager.getInstance().startGame(4, 0, "id", "id", figureManager);
         Assert.assertEquals(TurnPhase.CHOOSECARD, GameManager.getInstance().getCurrentTurnPhase());
         Assert.assertEquals(0, GameManager.getInstance().getMyTurnNumber());
         Assert.assertEquals(0, GameManager.getInstance().getCurrentTurnPlayerNumber());
@@ -57,7 +82,6 @@ public class GamemanagerTest {
 
     @Test
     public void nextTurnTestOnce() {
-        GameManager.getInstance().startGame(4, 0, "id","id", figureManager);
         GameManager.getInstance().nextTurn();
         Assert.assertEquals(1, GameManager.getInstance().getCurrentTurnPlayerNumber());
         Assert.assertEquals(TurnPhase.CHOOSECARD, GameManager.getInstance().getCurrentTurnPhase());
@@ -65,7 +89,6 @@ public class GamemanagerTest {
 
     @Test
     public void nextTurnTestTwice() {
-        GameManager.getInstance().startGame(4, 0, "id","id", figureManager);
         GameManager.getInstance().nextTurn();
         GameManager.getInstance().nextTurn();
         Assert.assertEquals(2, GameManager.getInstance().getCurrentTurnPlayerNumber());
@@ -74,7 +97,6 @@ public class GamemanagerTest {
 
     @Test
     public void nextTurnTestWholeRound() {
-        GameManager.getInstance().startGame(4, 0, "id","id", figureManager);
         GameManager.getInstance().nextTurn();
         GameManager.getInstance().nextTurn();
         GameManager.getInstance().nextTurn();
@@ -85,20 +107,17 @@ public class GamemanagerTest {
 
     @Test
     public void isItMyTurnTestTrue() {
-        GameManager.getInstance().startGame(4, 0, "id","id", figureManager);
         Assert.assertEquals(true, GameManager.getInstance().isItMyTurn());
     }
 
     @Test
     public void isItMyTurnTestFalse() {
-        GameManager.getInstance().startGame(4, 0, "id","id", figureManager);
         GameManager.getInstance().nextTurn();
         Assert.assertEquals(false, GameManager.getInstance().isItMyTurn());
     }
 
     @Test
     public void cardGotPlayedTest() {
-        GameManager.getInstance().startGame(4, 0, "id","id", figureManager);
         GameManager.getInstance().cardGotPlayed(card);
         Assert.assertEquals(TurnPhase.CHOOSEFIGURE, GameManager.getInstance().getCurrentTurnPhase());
         Assert.assertEquals(card, GameManager.getInstance().getSelectedCard());
@@ -106,19 +125,15 @@ public class GamemanagerTest {
 
     @Test
     public void testUpdateBoardSingleFigure(){
-        GameManager.getInstance().startGame(4, 0, "id", "id", figureManager);
+
         GameManager.getInstance().nextTurn();
-        Figure figure1 = new Figure();
-        Field field1 = new Field();
-        UpdateBoardPayload updateBoardPayload = new UpdateBoardPayload(1, -1, 3, -1, 4, 0, "id", "id");
         LastTurn expected = new LastTurn(figure1, null, field1, null);
-        when(figureManager.getFigureWithID(1)).thenReturn(figure1);
-        when(playingField.getFieldWithID(3)).thenReturn(field1);
 
-        GameManager.getInstance().updateBoard(updateBoardPayload);
 
-        verify(figureManager, times(1)).getFigureWithID(1);
-        verify(playingField, times(1)).getFieldWithID(3);
+        GameManager.getInstance().updateBoard(updateBoardPayloadSingleFigure);
+
+        verify(figureManager, times(1)).getFigureWithID(figure1.getId());
+        verify(playingField, times(1)).getFieldWithID(field1.getFieldID());
         verify(playingField, times(1)).moveFigureToField(figure1, field1);
         LastTurn actualLastTurn = GameManager.getInstance().getLastTurn();
         Assert.assertEquals(expected.getFigure1(), actualLastTurn.getFigure1());
@@ -130,23 +145,14 @@ public class GamemanagerTest {
     public void testUpdateBoardTwoFigures(){
         GameManager.getInstance().startGame(4, 0, "id", "id", figureManager);
         GameManager.getInstance().nextTurn();
-        Figure figure1 = new Figure();
-        Figure figure2 = new Figure();
-        Field field1 = new Field();
-        Field field2 = new Field();
-        UpdateBoardPayload updateBoardPayload = new UpdateBoardPayload(1, 9, 3, 12, 4, 0, "id", "id");
         LastTurn expected = new LastTurn(figure1, figure2, field1, field2);
-        when(figureManager.getFigureWithID(1)).thenReturn(figure1);
-        when(playingField.getFieldWithID(3)).thenReturn(field1);
-        when(figureManager.getFigureWithID(9)).thenReturn(figure2);
-        when(playingField.getFieldWithID(12)).thenReturn(field2);
 
-        GameManager.getInstance().updateBoard(updateBoardPayload);
+        GameManager.getInstance().updateBoard(updateBoardPayloadTwoFigures);
 
-        verify(figureManager, times(1)).getFigureWithID(1);
-        verify(playingField, times(1)).getFieldWithID(3);
-        verify(figureManager, times(1)).getFigureWithID(9);
-        verify(playingField, times(1)).getFieldWithID(12);
+        verify(figureManager, times(1)).getFigureWithID(figure1.getId());
+        verify(playingField, times(1)).getFieldWithID(field1.getFieldID());
+        verify(figureManager, times(1)).getFigureWithID(figure2.getId());
+        verify(playingField, times(1)).getFieldWithID(field2.getFieldID());
         verify(playingField, times(1)).moveFigureToField(figure1, field1);
         verify(playingField, times(1)).moveFigureToField(figure2, field2);
         LastTurn actualLastTurn = GameManager.getInstance().getLastTurn();
