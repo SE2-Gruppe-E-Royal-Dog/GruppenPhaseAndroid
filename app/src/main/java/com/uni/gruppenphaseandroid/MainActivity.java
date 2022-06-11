2e1d962192152f9a23cedbe8b30b8b030756e544
claudialei <100758060+claudialei@users.noreply.github.com>
2022-06-11 21:23:39 +0200

    added icon in toolbar

--- app/src/main/java/com/uni/gruppenphaseandroid/MainActivity.java
+++ app/src/main/java/com/uni/gruppenphaseandroid/MainActivity.java
@@ -80,6 +80,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(" CAT ROYAL");
+        getSupportActionBar().setIcon(R.drawable.ic_action_cat);
         bindService();
         doRegisterReceiver();
         setPlayerIdInCardView();
