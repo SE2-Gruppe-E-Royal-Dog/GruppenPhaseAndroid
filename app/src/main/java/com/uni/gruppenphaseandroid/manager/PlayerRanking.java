a04da3b77b6c470114d27bef11a32dc71421cb27
Lukas Krall <lukaskr@edu.aau.at>
2022-06-12 22:34:42 +0200

    Klasse PLayerRanking erstellt. Konstruktor implementiert, hier wird gezählt wie viele Figuren jede Farbe im Zielbereich hat. Zählmethode muss noch berechnet werden.

---
+++ app/src/main/java/com/uni/gruppenphaseandroid/manager/PlayerRanking.java
@@ -0,0 +1,50 @@
+package com.uni.gruppenphaseandroid.manager;
+
+import com.uni.gruppenphaseandroid.playingfield.GoalField;
+import com.uni.gruppenphaseandroid.playingfield.PlayingField;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PlayerRanking {
+
+    private LinkedList<String> names;
+    private LinkedList<Integer> points;
+
+    public PlayerRanking() {
+        GameManager gameManager = GameManager.getInstance();
+        PlayingField playingField = gameManager.getPlayingField();
+
+        names = gameManager.getPlayerNames();
+        points = new LinkedList<>();
+        points.addLast(calcPoints(playingField.getGreenStartingField().getNextGoalField()));
+        if (names.get(1) != null) {
+            points.addLast(calcPoints(playingField.getYellowStartingField().getNextGoalField()));
+        }
+        if (names.get(2) != null) {
+            points.addLast(calcPoints(playingField.getRedStartingField().getNextGoalField()));
+        }
+        if (names.get(3) != null) {
+            points.addLast(calcPoints(playingField.getBlueStartingField().getNextGoalField()));
+        }
+
+        sortLists();
+    }
+
+    private int calcPoints(GoalField goalField) {
+        return 0;
+    }
+
+    private void sortLists() {
+
+    }
+
+    private void addRanksToNames() {
+
+    }
+
+    public LinkedList<String> getRankedList() {
+        return names;
+    }
+}
